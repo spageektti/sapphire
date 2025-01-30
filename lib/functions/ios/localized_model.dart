3d5869fb91a113747abab404721c4f9c30fa08d7
Wiktor Perskawiec <git@spageektti.cc>
2025-01-30 21:10:29 +0100

    add function

---
+++ lib/functions/ios/localized_model.dart
@@ -0,0 +1,86 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Wiktor Perskawiec <contact@spageektti.cc>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+/*
+* Copyright (C) 2024 Wiktor Perskawiec <contact@spageektti.cc>
+
+? This program is free software: you can redistribute it and/or modify
+? it under the terms of the GNU General Public License as published by
+? the Free Software Foundation, either version 3 of the License, or
+? (at your option) any later version.
+
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+/* 
+! To contribute, please read the CONTRIBUTING.md file in the root of the project.
+? It contains important information about the project structure, code style, suggested VSCode extensions, and more.
+*/
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:sapphire/widgets/info_modal_bottom_sheet.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+
+class LocalizedModelWidget extends StatefulWidget {
+  const LocalizedModelWidget({super.key});
+
+  @override
+  _LocalizedModelWidgetState createState() => _LocalizedModelWidgetState();
+}
+
+class _LocalizedModelWidgetState extends State<LocalizedModelWidget> {
+  String _localizedModel = "";
+
+  Future<void> getLocalizedModel() async {
+    try {
+      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      setState(() {
+        _localizedModel = iosInfo.localizedModel;
+      });
+    } catch (e) {
+      setState(() {
+        _localizedModel = context.tr('unknownOrNotIosNotice');
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_localizedModel == "") getLocalizedModel();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(context.tr('localizedModelLongName')),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.info_outline),
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) => InfoModalBottomSheet(
+                      name: context.tr('localizedModelLongName'),
+                      description: context.tr('localizedModelLongDescription'),
+                      author: 'Wiktor Perskawiec (spageektti)',
+                      authorUrl: 'https://spageektti.cc'));
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_localizedModel, style: const TextStyle(fontSize: 24)),
+          ],
+        ),
+      ),
+    );
+  }
+}
