afecdc49e4f536d24de02f5dda6a4cf8974dd9ab
Wiktor Perskawiec <git@spageektti.cc>
2025-01-31 19:22:29 +0100

    add function

---
+++ lib/functions/web/web_vendor_sub.dart
@@ -0,0 +1,87 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Wiktor Perskawiec <contact@spageektti.cc>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+/*
+* Copyright (C) 2024 Wiktor Perskawiec <contact@spageektti.cc>
+
+? This program is free software: you can redistribute it and/or modify
+? it under the terms of the GNU General Public License as published by
+? the Free Software Foundation, either version 3 of the License, or
+? (at your option) any later version.
+
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+/* 
+! To contribute, please read the CONTRIBUTING.md file in the root of the project.
+? It contains important information about the project structure, code style, suggested VSCode extensions, and more.
+*/
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:sapphire/widgets/info_modal_bottom_sheet.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+
+class WebVendorSubWidget extends StatefulWidget {
+  const WebVendorSubWidget({super.key});
+
+  @override
+  _WebVendorSubWidgetState createState() => _WebVendorSubWidgetState();
+}
+
+class _WebVendorSubWidgetState extends State<WebVendorSubWidget> {
+  String _vendorSub = "";
+
+  Future<void> getVendorSub() async {
+    try {
+      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      WebBrowserInfo webBrowserInfo = await deviceInfo.webBrowserInfo;
+      setState(() {
+        _vendorSub = webBrowserInfo.vendorSub ??
+            context.tr('unknownOrNotWebBrowserNotice');
+      });
+    } catch (e) {
+      setState(() {
+        _vendorSub = context.tr('unknownOrNotWebBrowserNotice');
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_vendorSub == "") getVendorSub();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(context.tr('webVendorSubLongName')),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.info_outline),
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) => InfoModalBottomSheet(
+                      name: context.tr('webVendorSubLongName'),
+                      description: context.tr('webVendorSubLongDescription'),
+                      author: 'Wiktor Perskawiec (spageektti)',
+                      authorUrl: 'https://spageektti.cc'));
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_vendorSub, style: const TextStyle(fontSize: 24)),
+          ],
+        ),
+      ),
+    );
+  }
+}
