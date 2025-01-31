3dc1ce0461b4b0ecc89958499602c4a2de73f213
Wiktor Perskawiec <git@spageektti.cc>
2025-01-31 20:09:37 +0100

    add function

---
+++ lib/functions/macos/macos_memory_size.dart
@@ -0,0 +1,87 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Wiktor Perskawiec <contact@spageektti.cc>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+/*
+* Copyright (C) 2024 Wiktor Perskawiec <contact@spageektti.cc>
+
+? This program is free software: you can redistribute it and/or modify
+? it under the terms of the GNU General Public License as published by
+? the Free Software Foundation, either version 3 of the License, or
+? (at your option) any later version.
+
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+/* 
+! To contribute, please read the CONTRIBUTING.md file in the root of the project.
+? It contains important information about the project structure, code style, suggested VSCode extensions, and more.
+*/
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:sapphire/widgets/info_modal_bottom_sheet.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+
+class MacOSMemorySizeWidget extends StatefulWidget {
+  const MacOSMemorySizeWidget({super.key});
+
+  @override
+  _MacOSMemorySizeWidgetState createState() => _MacOSMemorySizeWidgetState();
+}
+
+class _MacOSMemorySizeWidgetState extends State<MacOSMemorySizeWidget> {
+  String _memorySize = "";
+
+  Future<void> getMacOSMemorySize() async {
+    try {
+      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      MacOsDeviceInfo macInfo = await deviceInfo.macOsInfo;
+      setState(() {
+        _memorySize =
+            "${(macInfo.memorySize / (1024 * 1024 * 1024)).toStringAsFixed(2)} GB";
+      });
+    } catch (e) {
+      setState(() {
+        _memorySize = context.tr('unknownOrNotMacOSNotice');
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_memorySize == "") getMacOSMemorySize();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(context.tr('macOSMemorySizeLongName')),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.info_outline),
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) => InfoModalBottomSheet(
+                      name: context.tr('macOSMemorySizeLongName'),
+                      description: context.tr('macOSMemorySizeLongDescription'),
+                      author: 'Wiktor Perskawiec (spageektti)',
+                      authorUrl: 'https://spageektti.cc'));
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_memorySize, style: const TextStyle(fontSize: 24)),
+          ],
+        ),
+      ),
+    );
+  }
+}
